0bad792af520dfe62ac1ff7f76aa2beccd15ee6c
shreetishrestha <shreeti.shrestha@cotiviti.com>
2021-04-26 16:16:12 +0545

    Basic map rendered with state base layer

--- src/app.js
+++ src/app.js
@@ -1,15 +1,29 @@
 import * as d3 from 'd3'
 
-(async function () {
-  // Selecting and appending elements
-  d3.select('#root')
-    .append('h5')
-    .append('text')
-    .text(`D3 version: ${d3.version}`)
+let margin = {
+  top: 10,
+  right: 20,
+  bottom: 10,
+  left: 20,
+}, width = 1200 - margin.left - margin.right,
+  height = 800 - margin.top - margin.bottom
 
-  // Loading external data
-  const dataset = await d3.csv('/data/sample.csv')
-  dataset.forEach((data) => {
-    console.log(data)
-  })
-}) ();
+let svg = d3.select('#root')
+  .append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+
+svg = svg.append('g')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+d3.json('/data/us-states.json').then(function (data) {
+  console.info(data)
+  const aProjection = d3.geoAlbersUsa().translate([600,300])
+  const geoPath = d3.geoPath().projection(aProjection);
+  d3.select('svg').selectAll('path').data(data.features)
+    .enter()
+    .append('path')
+    .attr('d', geoPath)
+    .attr('class', 'us-states');
+})
+
